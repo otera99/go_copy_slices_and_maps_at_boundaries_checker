125a981a8dcd2ffd7d9269a6b1dadbf052fa4d44
Tatsuya Terao <tt@connectfree.co.jp>
2020-09-03 13:05:52 +0900

    引数のスライスで受け取ったスライスもしくはマップがそのままフィールドに保存されている関数の処理

--- go_copy_slices_and_maps_at_boundaries_checker.go
+++ go_copy_slices_and_maps_at_boundaries_checker.go
@@ -39,35 +39,31 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 	mFunc := map[types.Object]bool{}
 	mSlice := map[types.Object]bool{}
+	mPair := map[Pair]bool{}
 
 	// 引数のスライスで受け取ったスライスもしくはマップがそのままフィールドに保存されている関数があるかを調べるパート
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch t := n.(type) {
 		case *ast.FuncDecl:
+			funcObj := pass.TypesInfo.ObjectOf(t.Name)
+
+			var recvObj types.Object
 			if t.Recv != nil && t.Recv.List != nil {
-				fmt.Println(reflect.TypeOf(t.Recv.List[0].Type))
-				switch u := t.Recv.List[0].Type.(type) {
-				case *ast.StarExpr :
-					switch v := u.X.(type) {
-					case *ast.Ident :
-						recvObj := pass.TypesInfo.ObjectOf(v)
-						fmt.Println(recvObj)
-					}
+				// fmt.Println(reflect.TypeOf(t.Recv.List[0].Type))
+				if t.Recv.List[0].Names != nil {
+					recvObj = pass.TypesInfo.ObjectOf(t.Recv.List[0].Names[0])
 				}
 			}
+			fmt.Println(recvObj)
 			mArgUsed := map[types.Object]bool{}
 			mArgNum := map[types.Object]int{} 
 			for i, arg := range t.Type.Params.List {
+				fmt.Print(arg)
 				fmt.Println(reflect.TypeOf(arg.Type))
-				switch u := arg.Type.(type) {
-				case *ast.ArrayType :
-					switch v := u.Elt.(type) {
-					case *ast.Ident :
-						argObj := pass.TypesInfo.ObjectOf(v)
-						// fmt.Println(argObj)
-						mArgUsed[argObj] = true
-						mArgNum[argObj] = i
-					}
+				if arg.Names != nil {
+					argObj := pass.TypesInfo.ObjectOf(arg.Names[0])
+					mArgUsed[argObj] = true
+					mArgNum[argObj] = i
 				}
 			}
 
@@ -77,15 +73,36 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				switch u := stmt.(type) {
 				case *ast.AssignStmt:
 					if u.Lhs != nil && u.Rhs != nil {
-						// switch v := u.Lhs[0].(type) {
-							
-						// }
+						// fmt.Println(u.Lhs[0])
+						// fmt.Println(reflect.TypeOf(u.Lhs[0]))
+						var stObj types.Object
+						switch v := u.Lhs[0].(type) {
+						case *ast.SelectorExpr :
+							//fmt.Println(reflect.TypeOf(v.X))
+							switch w := v.X.(type) {
+							case *ast.Ident :
+								stObj = pass.TypesInfo.ObjectOf(w)
+								fmt.Println(stObj)
+							}
+						}
+						// u.Rhs[0] が *ast.CallExpr のエッヂケースにも対応する(testdate の b.go)
+						var sliceObj types.Object
+						// fmt.Println(u.Rhs[0])
+					    fmt.Println(reflect.TypeOf(u.Rhs[0]))
+						switch v := u.Rhs[0].(type) {
+						case *ast.Ident :
+							sliceObj = pass.TypesInfo.ObjectOf(v)
+						}
+						fmt.Println(sliceObj)
+						if stObj != nil && sliceObj != nil && recvObj == stObj && mArgUsed[sliceObj] {
+							check = true
+							mPair[Pair{funcObj, mArgNum[sliceObj]}] = true
+						}
 					}
 				}
 			}
-			obj := pass.TypesInfo.ObjectOf(t.Name)
 			// fmt.Println(obj)
-			mFunc[obj] = check
+			mFunc[funcObj] = check
 		}
 	})
 
@@ -94,17 +111,18 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch t := n.(type) {
 		case *ast.CallExpr:
-			switch u := t.Fun.(type) {
-			case *ast.SelectorExpr :
-				obj := pass.TypesInfo.ObjectOf(u.Sel)
-				//fmt.Println(obj)
-				if(mFunc[obj]) {
-					// 処理を書く
-					for _, arg := range t.Args {
-						fmt.Println(arg)
-					}
-				}
-			}
+			// switch u := t.Fun.(type) {
+			// case *ast.SelectorExpr :
+			// 	obj := pass.TypesInfo.ObjectOf(u.Sel)
+			// 	//fmt.Println(obj)
+			// 	if(mFunc[obj]) {
+			// 		// 処理を書く
+			// 		for i, arg := range t.Args {
+			// 			fmt.Println(arg)
+			// 			// fmt.Println(i)
+			// 		}
+			// 	}
+			// }
 		case *ast.AssignStmt:
 			if t.Lhs != nil && t.Rhs != nil {
 				switch u := t.Lhs[0].(type) {
